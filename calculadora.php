716271896a73652577bf6cf67a31c33559a870c7
jeferson favero <jefersonvieirafavero@gmail.com>
2015-04-07 21:48:08 -0300

    atualizando html

--- calculadora.php
+++ calculadora.php
@@ -1,1 +1,22 @@
+<html>
+<head>
+</head>
+<body>
+<center><h1> Calculadora </h1></center>
+
+<form action="calculadora.php" method="post">
+
+<input type="numero1">
+<br>
+<input type="numero2">
+<br>
+<input type=submit value="somar">
+<br>
+<input type="resultado">
+<br>
+
+</form>
+
+</body>
+</html>
 
